61dd5c7ffeccb6cccfb2ce787ae0922197b856bf
AlexKanivets <al.kanivets@mail.ru>
2021-12-22 01:14:48 +0300

    add SpringBoot

---
+++ src/main/java/web/crud/dao/RoleDao.java
@@ -0,0 +1,12 @@
+package web.crud.dao;
+
+import web.crud.model.Role;
+
+import java.util.HashSet;
+import java.util.List;
+
+public interface RoleDao {
+    List <Role> getAllRoles();
+    Role getRoleByName(String name);
+    HashSet<Role> getSetOfRoles(String[] roleNames);
+}
