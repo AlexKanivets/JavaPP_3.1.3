9347a5209d979dcbc6cc9bfec328f690ec37a7af
AlexKanivets <al.kanivets@mail.ru>
2021-12-26 03:39:16 +0300

    add bootstrap

--- src/main/java/web/crud/controller/AdminController.java
+++ src/main/java/web/crud/controller/AdminController.java
@@ -1,6 +1,7 @@
 package web.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,8 +23,10 @@ public class AdminController {
     }
 
     @GetMapping()
-    public String getUsers(Model model) {
+    public String getUsers(@AuthenticationPrincipal User user, Model model) {
+        model.addAttribute("user", user);
         model.addAttribute("users", userService.getAllUser());
+        model.addAttribute("roles", roleService.getAllRoles());
         return "index";
     }
 
